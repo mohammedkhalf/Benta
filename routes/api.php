86412dd39efe6b58dc88b0a2c274c5329f3dd5c5
khalf <mohamed.khalaf@cowpay.me>
2024-02-06 23:42:18 +0200

    Develop User Auth API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\UsersAuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/** ---------Register and Login ----------- */
+Route::controller(UsersAuthController::class)->group(function(){
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+});
+
